2a4433e6c23ee90c334975ba9a68f0c61d5af3f1
Cyberboyone <cyberboyone@users.noreply.github.com>
2026-09-15 14:56:15 +0100

    Initial commit - Haifan Shuabul Iman 1

---
+++ lib/data/sample_lessons.dart
@@ -0,0 +1,114 @@
+import '../models/lesson.dart';
+
+const List<Lesson> sampleLessons = [
+  Lesson(
+    title: "001 Shu'abul Iman - Karkasa Imani",
+    audioPath: "assets/audio/001 Shu'abul Iman 001 Karkasa Imani.ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "002 Shu'abul Iman - Gaskanta Samuwar Allah",
+    audioPath: "assets/audio/002 Shu'abul Iman 002 Gaskanta Samuwar Allah.ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "003 Shu'abul Iman - Gaskanta Manzanni",
+    audioPath: "assets/audio/003 Shu'abul Iman 003 Gaskanta Manzanni.ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "004 Shu'abul Iman - Gaskanta Littafan Allah",
+    audioPath: "assets/audio/004 Shu'abul Iman 004 Gaskanta Littafan Allah.ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "005 Shu'abul Iman - Gaskanta Kaddara",
+    audioPath: "assets/audio/005 Shu'abul Iman 005 Gaskanta Kaddara.ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "006 Shu'abul Iman - Gaskanta Ranar Karshe",
+    audioPath: "assets/audio/006 Shu'abul Iman 006 Gaskanta Ranar 'Karshe.ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "008 Shu'abul Iman - Gaskanta Taron Lahira",
+    audioPath: "assets/audio/008 Shu'abul Iman 008 Gaskanta Taron Lahira.ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "009 Shu'abul Iman - Gaskanta Aljanna Da Wuta",
+    audioPath: "assets/audio/009 Shu'abul Iman 009 Gaskanta Aljanna Da Wuta.ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "010 Shu'abul Iman - Wajibcin Son Allah",
+    audioPath: "assets/audio/010 Shu'abul Iman 010 Wajibcin Son Allah.ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "011 Shu'abul Iman - Wajibcin Tsoron Allah",
+    audioPath: "assets/audio/011 Shu'abul Iman 011 Wajibcin Tsoron Allah.ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "012 Shu'abul Iman - Fatar Rahamar Allah",
+    audioPath: "assets/audio/012 Shu'abul Iman 012 Fatar Rahamar Allah.ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "014 Shu'abul Iman - Sharadin Cikakken Tawakkali",
+    audioPath: "assets/audio/014 Shu'abul Iman 014 Sharadin Cikakken Tawakkali.ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "015 Shu'abul Iman - Dogaro Ga Sababi",
+    audioPath: "assets/audio/015 Shu'abul Iman 015 Dogaro Ga Sababi.ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "016 Shu'abul Iman - Son Manzon Allah",
+    audioPath: "assets/audio/016 Shu'abul Iman 016 Son Manzon Allah.ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "017 Shu'abul Iman - Daukaka Darajan Annabi",
+    audioPath: "assets/audio/017 Shuabul_Iman_017_Daukaka_Darajan_Annabi.ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "018 Shu'abul Iman - Daukaka Darajan Annabawan Allah",
+    audioPath: "assets/audio/018_Shu'abul_Iman_018_Daukaka_Darajan_Annabawan_Allah_2.ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "019 Shu'abul Iman - Neman Ilmin Addini (1)",
+    audioPath: "assets/audio/Shu'abul Iman 019 Neman Ilmin Addini (1).ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "020 Shu'abul Iman - Neman Ilmin Addini (2)",
+    audioPath: "assets/audio/Shu'abul Iman 020 Neman Ilmin Addini (2).ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "021 Shu'abul Iman - Ya'da Ilmin Addini (1)",
+    audioPath: "assets/audio/Shu'abul Iman 021 Ya'da Ilmin Addini (1).ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "022 Shu'abul Iman - Ya'da Ilmin Addini (2)",
+    audioPath: "assets/audio/Shu'abul Iman 022 Ya'da Ilmin Addini (2).ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "023 Shu'abul Iman - Girmama Al-Qur'ani (1)",
+    audioPath: "assets/audio/Shu'abul Iman 023 Girmama Al-Qur'ani (1).ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "024 Shu'abul Iman - Girmama Al-Qur'ani (2)",
+    audioPath: "assets/audio/Shu'abul Iman 024 Girmama Al-Qur'ani (2).ogg",
+    course: "Shu'abul Iman",
+  ),
+];
